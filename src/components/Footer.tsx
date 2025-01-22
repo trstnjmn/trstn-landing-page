cb01ed1a7243b2fce97e3af2f184da1a8c8c539e
trstn <t.jamin@bitexpert.de>
2025-01-22 11:48:47 +0100

    fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -92,6 +92,7 @@ export const Footer = () => {
         <div className="flex flex-col col-span-2 md:col-span-1  gap-2">
           <h5 className="text-sm">
             Landing page made by{" "} </h5>
+          <h5 className="text-xs">
             <a
                 rel="noreferrer noopener"
                 target="_blank"
@@ -100,6 +101,7 @@ export const Footer = () => {
             >
               Torsten Jamin
             </a>
+          </h5>
         </div>
       </section>
     </footer>

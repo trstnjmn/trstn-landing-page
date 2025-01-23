9823db053d66dc0c72cc0caa3d1ecbff96f4600c
trstn <t.jamin@bitexpert.de>
2025-01-23 15:41:29 +0100

    add scroll effect to header

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import { useTheme } from "@/components/theme-provider.tsx";
 
 const goToTop = () => {
@@ -12,28 +13,48 @@ const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
   const element = document.getElementById(targetId);
   if (element) {
     const yOffset = -120; // Offset in Pixeln (Anpassung nach Bedarf)
-    const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+    const y = element.getBoundingClientRect().top + window.screenY + yOffset;
     window.scrollTo({ top: y, behavior: 'smooth' });
   }
 };
 
-
 export const Navbar = () => {
-  // useTheme Hook wird jetzt innerhalb der Komponente aufgerufen
   const { theme, setTheme } = useTheme();
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [lastScrollY, setLastScrollY] = useState(0);
 
   const handleThemeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setTheme(event.target.value as "light" | "dark");
   };
 
-  // Bestimme den initialen Wert für das Dropdown
   const themeValue = theme === "system"
       ? (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light")
       : theme;
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+
+      if (window.scrollY < lastScrollY) {
+        setIsScrolled(false);
+      }
+      setLastScrollY(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
   return (
-      <header className="sticky top-0 z-40 w-full bg-card">
-        <div className="md:py-4 py-6 md:px-8 w-[95vw] flex justify-between flex flex-col md:flex-row gap-4">
+      <header className={`sticky top-0 z-40 w-full bg-card transition-all ${isScrolled ? "transform -translate-y-full" : ""}`}>
+        <div className="md:py-4 py-6 md:px-8 w-[95vw] flex justify-between flex-col md:flex-row gap-4">
           <div>
             <h1 className="ml-8 my-auto font-bold md:text-4xl text-base text-snes-nature dark:text-snes-grey">
               <a rel="noreferrer noopener" onClick={goToTop}>
@@ -43,9 +64,9 @@ export const Navbar = () => {
           </div>
 
           <div className="flex md:flex-row flex-col gap-8 ml-8">
-            <div className="snes-form-group w-[400px]">
+            <div className="snes-form-group md:w-[400px] w-full">
               <div className="snes-input is-success dark:bg-snes-grey">
-                <select onChange={handleChange}>
+                <select onChange={handleChange} className="text-sm">
                   <option value="" disabled selected>Select Navi</option>
                   <option value="hero">Start</option>
                   <option value="skills">Skills</option>
@@ -57,7 +78,7 @@ export const Navbar = () => {
               </div>
             </div>
 
-            <div className="snes-form-group w-[220px]">
+            <div className="snes-form-group md:w-[220px] w-full">
               <div className="snes-input is-warning dark:bg-snes-grey">
                 <select onChange={handleThemeChange} value={themeValue}>
                   <option value="light">Light</option>
@@ -67,7 +88,7 @@ export const Navbar = () => {
             </div>
           </div>
         </div>
-        <hr className="bg-black h-1"/>
+        <hr className="bg-black h-1" />
       </header>
   );
 };
